57bde87e1bb737e9f0114e42f181b81c5f049250
Null <Null>
2021-06-30 23:06:00 +0530

    First 3 questions

--- Q1.py
+++ Q1.py
@@ -1,13 +1,29 @@
 #Mississippi (program1)
-str1=input("Please enter a string: ")
-str2=str1.lower()   #making string case insensitive.
-list1=list(str2)
-new=[]
-for i in list1:
-    if i not in new:
-        new.append(i)
-        count=0
-        for j in range(len(list1)):
-            if i==list1[j]:
-                count+=1
-        print(f"{i} = {count}")
+print("Enter String")
+
+def fun(s):
+    return -1*cnt[s]
+
+s = input()
+
+cnt = {}
+
+
+
+for x in s:
+    if x in cnt:
+        cnt[x]+=1
+    else:
+        cnt[x] = 1
+
+l = list(cnt)
+l = sorted(l , key = fun)
+
+for x in l:
+    print(str(x) + "=" + str(cnt[x]))
+
+
+
+
+
+
